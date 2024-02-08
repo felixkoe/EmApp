9b4c9c839bdd0cab5b62ae2f52d52d14a447d33a
Felix Koelling <felixkoelling@hotmail.de>
2024-02-08 23:36:42 +0100

    init commit

---
+++ GccApplication6/defines.h
@@ -0,0 +1,18 @@
+#ifndef INCFILE2_H_
+#define INCFILE2_H_
+
+#define F_CPU 16000000UL
+
+#define e1 0x1
+#define e2 0x2
+#define e3 0x4
+#define e4 0x8
+#define e5 0x10
+#define e6 0x20
+#define e7 0x40
+#define e8 0x80
+
+
+
+
+#endif /* INCFILE2_H_ */
---
+++ GccApplication6/timers.h
@@ -0,0 +1,12 @@
+#ifndef INCFILE1_H_
+#define INCFILE1_H_
+
+struct 
+
+void declareTimer();
+
+void startTimer();
+
+void deleteTimer();
+
+#endif /* INCFILE1_H_ */
